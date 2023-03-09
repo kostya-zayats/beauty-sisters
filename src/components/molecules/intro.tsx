213a0e7f8a9e8fff6e4f7365d50cbed62e7db58b
Eugen Zayats <e.zayats@richbrains.net>
2023-03-09 18:15:40 +0300

    update

--- src/components/molecules/intro.tsx
+++ src/components/molecules/intro.tsx
@@ -1,25 +1,32 @@
 import { EnrollButton } from '@/components/atoms';
+import { Montserrat } from '@next/font/google';
 import React from 'react';
 
-export const Intro = () => {
-  return (
-    <div className='main-intro'>
-      <div className='main-intro-description'>
-        <h1>
-          студия красоты <span>beauty sisters</span>
-        </h1>
-        <ul className='main-intro-description-list'>
-          <li>косметология</li>
-          <li>подология</li>
-          <li>массаж</li>
-          <li>прокол ушей INVERNESS</li>
-        </ul>
-        <div className='main-intro-description-btn-enroll-wrap'>
-          <EnrollButton />
-        </div>
-      </div>
+// config font
+const montserrat = Montserrat({
+    weight: '400',
+    subsets: ['latin'],
+});
 
-      <div className='main-intro-bg-img'></div>
-    </div>
-  );
+export const Intro = () => {
+    return (
+        <div className='main-intro'>
+            <div className='main-intro-description'>
+                <h1>
+                    студия красоты <span>beauty sisters</span>
+                </h1>
+                <ul className={`main-intro-description-list ${montserrat.className}`}>
+                    <li>косметология</li>
+                    <li>подология</li>
+                    <li>массаж</li>
+                    <li>прокол ушей INVERNESS</li>
+                </ul>
+                <div className='main-intro-description-btn-enroll-wrap'>
+                    <EnrollButton />
+                </div>
+            </div>
+
+            <div className='main-intro-bg-img'></div>
+        </div>
+    );
 };
